57bbc5e98cd4ea2ba29c2cfbc97c650c896f6b96
Ori <orinurieli1@gmail.com>
2022-12-28 19:54:57 +0200

    added comments

--- Data-Structures-Ex2/main.cpp
+++ Data-Structures-Ex2/main.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello Sweed";
+	// a
+	// Init() - initiate the ATD with no bridges on the m roads
+	// worst case - O(m)
+
+
+	// b
+	// AddBridge(float h, int r) - add bridge with height h on road r
+	// worst case - O(logm)
+
+
+	// c
+	// WhiceRoad(float h) - prints number of road for truck which it's height <= bridge's height
+	// if multiple roads, prints the heighest bridge the truck can go under
+	// if the truck is too high, print 0
+	// worst case - teta(logm)
+
+
+	// d
+	// Print(int r) - prints all bridges heights on the road
+	// Linear complex by num of roads
 
 
 	return 0;
